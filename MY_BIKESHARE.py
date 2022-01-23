097278c1f1a0c3c0d1c8603104de23779ed76165
Isoken Ologun <isoken45@gmail.com>
2022-01-22 23:18:59 -0700

    Changed variable name for time statistics

--- MY_BIKESHARE.py
+++ MY_BIKESHARE.py
@@ -99,7 +99,7 @@ def time_stats(df):
 
     # display the most common start hour
     common_hour = df['hour'].mode()[0]
-    print('The most common hour of travel is {}'.format(common_hour))
+    print('The most common travel hour is {}'.format(common_hour))
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
